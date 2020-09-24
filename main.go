2ea66e322f69c55e0e6f2e2e23ae249a519e28f9
FFFFDDDD25 <phone993825@gmail.com>
2020-09-24 20:24:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	
+	db, err1 := sql.Open("mysql", "dave.gan:12345678@/movie_database")
+	if(err1 != nil){
+		w.Write("sql open error")
+		return
+	}
+	
+	result, err2 := db.Exec(
+	    "INSERT INTO TestMovie (name) VALUES ('Dave Gan')"
+	)
+	
+	if(err2 != nil){
+		w.Write("sql insert error")
+		return
+	}
+	
 	w.Write([]byte("Hello!"))
+	return
 }
 func WorldHandler(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("World!"))
