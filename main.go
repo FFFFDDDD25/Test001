0d1ccbe5bd59105487d0f7d1a1f6221dd017c2c5
FFFFDDDD25 <phone993825@gmail.com>
2020-09-24 20:35:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	
 	db, err1 := sql.Open("mysql", "dave.gan:12345678@/movie_database")
 	if(err1 != nil){
-		w.Write("sql open error")
+		w.Write([]byte"sql open error")
 		return
 	}
 	
 	result, err2 := db.Exec("INSERT INTO TestMovie (name) VALUES ('Dave Gan')")
 	
 	if(err2 != nil){
-		w.Write("sql insert error")
+		w.Write([]byte"sql insert error")
 		return
 	}
 	
