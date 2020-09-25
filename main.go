49f36c7e4132d313cb82ad2f8a75023feb5a524f
FFFFDDDD25 <phone993825@gmail.com>
2020-09-26 07:28:18 +0800

    foo

--- main.go
+++ main.go
@@ -7,24 +7,9 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
+func HelloHandler_4(w http.ResponseWriter, req *http.Request) {
 
-	var err error
-	var db *sql.DB
-
-	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20:3306)/movie_database")
-	if err != nil {
-		w.Write([]byte(err.Error()))
-	}
-	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230:3306)/movie_database")
-	if err != nil {
-		w.Write([]byte(err.Error()))
-	}
-	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20)/movie_database")
-	if err != nil {
-		w.Write([]byte(err.Error()))
-	}
-	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230)/movie_database")
+	db, err := sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230)/movie_database")
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	}
@@ -34,11 +19,56 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte(err.Error()))
 	}
 
-	w.Write([]byte("Hello!!!"))
 	return
 }
+
+func HelloHandler_3(w http.ResponseWriter, req *http.Request) {
+
+	db, err := sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20)/movie_database")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	_, err = db.Exec("INSERT INTO TestMovie (name) VALUES ('Dave Gan')")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	return
+}
+
+func HelloHandler_2(w http.ResponseWriter, req *http.Request) {
+
+	db, err := sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230:3306)/movie_database")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	_, err = db.Exec("INSERT INTO TestMovie (name) VALUES ('Dave Gan')")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	return
+}
+
+func HelloHandler_1(w http.ResponseWriter, req *http.Request) {
+
+	db, err := sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20:3306)/movie_database")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	_, err = db.Exec("INSERT INTO TestMovie (name) VALUES ('Dave Gan')")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	return
+
+}
 func WorldHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("World!~~~@@@@@"))
+	w.Write([]byte("World!"))
 }
 func MainHandler(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Main Page!"))
@@ -46,7 +76,10 @@ func MainHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	r := http.NewServeMux()
-	r.HandleFunc("/hello", HelloHandler)
+	r.HandleFunc("/h1", HelloHandler_1)
+	r.HandleFunc("/h2", HelloHandler_2)
+	r.HandleFunc("/h3", HelloHandler_3)
+	r.HandleFunc("/h4", HelloHandler_4)
 	r.HandleFunc("/world", WorldHandler)
 	r.HandleFunc("/", MainHandler)
 
