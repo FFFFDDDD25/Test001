503e9ae942feb9588ba1758cdacec49680634492
FFFFDDDD25 <phone993825@gmail.com>
2020-09-26 01:51:29 +0800

    foo

--- main.go
+++ main.go
@@ -8,43 +8,33 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	//FFFFFDDDDOOOO~~!!!!))))))AAAAA
 
 	var err error
-	w.Write([]byte("00000"))
-	all := "11111"
 	var db *sql.DB
 
 	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20:3306)/movie_database")
 	if err != nil {
-		all += err.Error()
+		w.Write([]byte(err.Error()))
 	}
 	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230:3306)/movie_database")
 	if err != nil {
-		all += err.Error()
+		w.Write([]byte(err.Error()))
 	}
 	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(34.66.219.20)/movie_database")
 	if err != nil {
-		all += err.Error()
+		w.Write([]byte(err.Error()))
 	}
 	db, err = sql.Open("mysql", "dave.gan:12345678@tcp(35.194.153.230)/movie_database")
 	if err != nil {
-		all += err.Error()
+		w.Write([]byte(err.Error()))
 	}
 
-	all += "22222"
-	w.Write([]byte(all + "33333"))
-
-	return
-
-	//35.194.153.230
 	_, err = db.Exec("INSERT INTO TestMovie (name) VALUES ('Dave Gan')")
 	if err != nil {
 		w.Write([]byte(err.Error()))
-		return
 	}
 
-	w.Write([]byte("Hello!  sql insert done!!"))
+	w.Write([]byte("Hello!!!"))
 	return
 }
 func WorldHandler(w http.ResponseWriter, req *http.Request) {
