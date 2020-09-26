f5e3ab26ab1a6240dc43e89f8d56e25935665a23
FFFFDDDD25 <phone993825@gmail.com>
2020-09-26 23:38:15 +0800

    foo

--- main.go
+++ main.go
@@ -41,9 +41,15 @@ func HelloHandler_2(w http.ResponseWriter, req1 *http.Request) {
 	}
 	caps.AddChrome(chromeCaps)
 	// 启动chromedriver，端口号可自定义
-	_, err = selenium.NewChromeDriverService("/usr/bin/chromedriver", 9515, opts...)
+	_, err = selenium.NewChromeDriverService("chromedriver", 9515, opts...) //"/usr/bin/chromedriver"
 	if err != nil {
 		w.Write([]byte("1111" + err.Error()))
+
+		_, err = selenium.NewChromeDriverService("/chromedriver", 9515, opts...)
+		if err != nil {
+			w.Write([]byte("1122" + err.Error()))
+		}
+
 		return
 	}
 	// 调起chrome浏览器
