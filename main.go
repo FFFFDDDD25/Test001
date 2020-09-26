5a08ec2872e43f7808319610a9fff8b8a03f797f
FFFFDDDD25 <phone993825@gmail.com>
2020-09-26 22:20:02 +0800

    foo

--- main.go
+++ main.go
@@ -6,14 +6,68 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 
 	"cloud.google.com/go/storage"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
 )
 
 //gcloud sql connect movie-english-database --user=dave.gan --quiet
+
+func HelloHandler_2(w http.ResponseWriter, req1 *http.Request) {
+
+	var err error
+	opts := []selenium.ServiceOption{}
+	caps := selenium.Capabilities{
+		"browserName": "chrome",
+	}
+
+	// 禁止加载图片，加快渲染速度
+	imagCaps := map[string]interface{}{
+		"profile.managed_default_content_settings.images": 2,
+	}
+
+	chromeCaps := chrome.Capabilities{
+		Prefs: imagCaps,
+		Path:  "",
+		Args: []string{
+			"--headless", // 设置Chrome无头模式
+			"--no-sandbox",
+			"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7", // 模拟user-agent，防反爬
+		},
+	}
+	caps.AddChrome(chromeCaps)
+	// 启动chromedriver，端口号可自定义
+	_, err = selenium.NewChromeDriverService("/opt/google/chrome/chromedriver", 9515, opts...)
+	if err != nil {
+		log.Printf("Error starting the ChromeDriver server: %v", err)
+	}
+	// 调起chrome浏览器
+	webDriver, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", 9515))
+	if err != nil {
+		panic(err)
+	}
+	// 这是目标网站留下的坑，不加这个在linux系统中会显示手机网页，每个网站的策略不一样，需要区别处理。
+	webDriver.AddCookie(&selenium.Cookie{
+		Name:  "defaultJumpDomain",
+		Value: "www",
+	})
+	// 导航到目标网站
+	err = webDriver.Get("https://www.xvideos.com/")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load page: %s\n", err))
+	}
+
+	title, err := webDriver.Title()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get title: %s\n", err))
+	}
+	w.Write([]byte("0000" + title))
+}
 
 func HelloHandler_3(w http.ResponseWriter, req *http.Request) {
 
@@ -118,6 +172,7 @@ func main() {
 	r := http.NewServeMux()
 	r.HandleFunc("/h4", HelloHandler_4)
 	r.HandleFunc("/h3", HelloHandler_3)
+	r.HandleFunc("/h2", HelloHandler_2)
 	r.HandleFunc("/world", WorldHandler)
 	r.HandleFunc("/", MainHandler)
 
